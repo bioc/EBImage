3668afc5535cc12e4553accbeee40db99305a813
Oleg Sklyar <osklyar@ebi.ac.uk>
2006-07-18 10:21:06 +0000

    prompts to update to 1.3.101
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/branches/RELEASE_1_8/madman/Rpacks/EBImage@18871 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/init.R
+++ R/init.R
@@ -1,9 +1,26 @@
+updateEBImageDev <- function() {
+    install.packages("EBImage", repos="http://www.bioconductor.org/packages/1.9/bioc")
+    cat("\n\nPlease re-load the library!\n\n")
+}
+
+updateEBImage <- function() {
+    install.packages("EBImage", repos="http://www.ebi.ac.uk/huber-srv/data/Rrepos")
+    cat("\n\nPlease re-load the library!\n\n")
+}
+
 # Package initialization routine
 .onLoad <- function(lib, pkg) {
     ## use useDynLib("EBImage") in NAMESPACE instead
     ## library.dynam("EBImage", pkg, lib)
     ## cat("* EBImage of Bioconductor.org: help(EBImage) to get started...\n")
     require("methods")
+
+cat("\nWARNING: EBImage API has changed in the development release 1.3.x\n\nIt is strongly recommended to update EBImage\n\nCall 'updateEBImageDev()' to upgrade to the latest development version\nor download it from http://www.bioconductor.org/packages/1.9/bioc/\n\nCall 'updateEBImage()' to upgrade to the latest STABLE version\nor download it from http://www.ebi.ac.uk/~osklyar/projects/EBImage/\n\n")
+
+cat("Do you want to upgrade to dev version now [yes/no], default = 'no'? ")
+up <- readline()
+if (tolower(up) == "yes")
+    upgradeEBImage()
 }
 
 # shortcut to call library functions for EBImage
